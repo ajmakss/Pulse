6ab6dd64b1c8847e8f6b2d05de2f6f60da7506e1
Maksym <maxim.bichkiv@gmail.com>
2021-08-03 18:06:45 +0300

    Catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,59 @@ $(document).ready(function(){
       });
 });
               
+
+// Tabs
+
+function tabs (headerSelector, tabSelector, contentSelector, activeSelector, display = 'flex'){
+    const header = document.querySelector(headerSelector),
+          tab= document.querySelectorAll(tabSelector),
+          content = document.querySelectorAll(contentSelector);
+    
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tab.forEach(item => {
+            item.classList.remove(activeSelector);
+        });
+    }
+    
+    function showTabContent(i = 0) {
+        content[i].style.display = display;
+        tab[i].classList.add(activeSelector);
+    }
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if( target == e.target && 
+            (target.classList.contains(tabSelector.replace(/\./, "")) || 
+             target.parentNode.classList.contains(tabSelector.replace(/\./, "")) )) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item ) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+
+        }
+    });
+}
+
+    tabs('.catalog__tabs', '.catalog__tab', '.catalog__content',  'catalog__tab_active');
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
